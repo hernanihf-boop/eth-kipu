75d1fa6056536797df416e08c4069eee6cc8ee9a
hernanihf-boop <hernan.ihf@gmail.com>
2025-10-18 19:15:11 -0300

    update donations and oracle

--- contracts/donations V2/DonationsV2.sol
+++ contracts/donations V2/DonationsV2.sol
@@ -2,8 +2,9 @@
 pragma solidity >0.8.0;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IOracle} from "./IOracle.sol";
+//import {IOracle} from "./IOracle.sol";
 import {GreatInvestor} from "./GreatInvestor.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract DonationsV2 is GreatInvestor {
 
@@ -15,28 +16,36 @@ contract DonationsV2 is GreatInvestor {
 
     mapping (address => Balances) public balance;
 
-    IOracle public oracle;
+    AggregatorV3Interface public dataFeed;
 
     IERC20 immutable public USDC;
 
-    constructor(IOracle _oracle, IERC20 _usdc) GreatInvestor(msg.sender) {
-        oracle = _oracle;
+    event FeedSet(address indexed, uint256 timestamp);
+    event Donated(string indexed usdcOrEth, uint256 amountDonated, uint256 amountInUsdc);
+    event Extracted(address owner, uint256 valueEth, uint256 valueUsdc);
+
+    constructor(AggregatorV3Interface _dataFeed, IERC20 _usdc) GreatInvestor(msg.sender) {
+        dataFeed = _dataFeed; // 0x694AA1769357215DE4FAC081bf1f309aDC325306
         USDC = _usdc;
+        emit FeedSet(address(_dataFeed), block.timestamp);
     }
 
     function setFeeds(address _feed) external onlyOwner {
-        oracle = IOracle(_feed);
+        dataFeed = AggregatorV3Interface(_feed); // 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        emit FeedSet(_feed, block.timestamp);
     }
 
     function doeETH() external payable {
         int256 _latestAnswer = _getETHPrice();
         balance[msg.sender].eth += msg.value;
-        balance[msg.sender].total += ((msg.value)*uint256(_latestAnswer)); // 18 + 8 = 26
+        uint256 _donatedInUsdc = ((msg.value)*uint256(_latestAnswer)); // 18 + 8 = 26
+        balance[msg.sender].total += _donatedInUsdc;
         if (balance[msg.sender].total > 1000*100000000* 1 ether) {
             if (balanceOf(msg.sender) < 1) {
                 safeMint(msg.sender, "url");
             }
         }
+        emit Donated("ETH", msg.value, _donatedInUsdc);
     }
 
     function doeUSDC(uint256 _usdcAmount) external {
@@ -49,16 +58,22 @@ contract DonationsV2 is GreatInvestor {
                 safeMint(msg.sender, "url");
             }
         }
+        emit Donated("USDC", _usdcAmount, _usdcAmount);
     }
 
     function saque() external onlyOwner {
-        USDC.transfer(msg.sender, USDC.balanceOf(address(this)));
+        uint256 _valueEth = address(this).balance;
+        uint256 _valueUsdc = USDC.balanceOf(address(this));
+        USDC.transfer(msg.sender, _valueUsdc);
         // saca eth
-        (bool success,) = msg.sender.call{value:address(this).balance}("");
+        (bool success,) = msg.sender.call{value:_valueEth}("");
         if(!success) revert();
+        emit Extracted(msg.sender, _valueEth, _valueUsdc);
     }
 
     function _getETHPrice() private view returns(int256 _latestAnswer) {
-        return _latestAnswer = oracle.latestAnswer();
+        // return _latestAnswer = dataFeed.latestAnswer();
+        (,_latestAnswer,,,) = dataFeed.latestRoundData();
+        return _latestAnswer;
     }
 }
--- contracts/donations V2/Oracle.sol
+++ contracts/donations V2/Oracle.sol
@@ -1,11 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.8.0;
 
-import {IOracle} from "./IOracle.sol";
+//import {IOracle} from "./IOracle.sol";
+// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-//mock del oraculo
-contract Oracle is IOracle {
+
+contract Oracle {
+    /*
     function latestAnswer() external pure returns(int256) {
         return 411788170000;
+    }
+    */
+    function latestRoundData() 
+    external 
+    pure 
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) {
+        return (0, 411788170000, 0, 0, 0);
     }
 }
